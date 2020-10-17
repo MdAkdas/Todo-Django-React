56d9993f65cfb1ebd2629bcab4479274e95b57f9
MdAkdas <ahmadakdas07@gmail.com>
2020-10-17 22:51:41 +0530

    befor django+react deployment

--- todo_drf/settings.py
+++ todo_drf/settings.py
@@ -85,11 +85,14 @@ WSGI_APPLICATION = 'todo_drf.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'todo',
+        'USER': 'akdas',
+        'PASSWORD': 'akdas@1234',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
